cf4d54c5db3913e872bd3b992927c9e197e7a896
Heftwig <87965854+Heftwig@users.noreply.github.com>
2026-06-19 18:51:23 +0200

    Delete avatar_tools directory

--- avatar_tools/Editor/AvatarToolsWindow.cs
+++
@@ -1,5 +0,0 @@
-#if UNITY_EDITOR
-// Migration placeholder.
-// The AvatarToolsWindow implementation is intentionally located in UI_Master.cs.
-// This file remains so importing over an older package overwrites the duplicate class.
-#endif
